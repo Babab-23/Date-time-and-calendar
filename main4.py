875c05ff632b02b7141bcafc782f7bafe8b10bc1
Adrian <you@example.com>
2025-02-02 14:32:04 +0100

    fourth commit

---
+++ main4.py
@@ -0,0 +1,8 @@
+import calendar
+
+# List of month names
+months = list(calendar.month_name)[1:]  # Skip the first empty string
+
+# Display all the month's names
+for month in months:
+    print(month)
